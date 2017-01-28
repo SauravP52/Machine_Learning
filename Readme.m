d17c7c9b19f0e0bd8ea122d1a27c756b9aca4f01
Saurav Pradhan <sauravpradhan08@hotmail.com>
2017-01-28 12:37:00 +0530

    Added KNN.py and Readme.m

---
+++ Readme.m
@@ -0,0 +1,17 @@
+###-------------------------------------------------------------------##
+This repository contains the algorithmic practices of Machine Learning
+using simple algorithm such as KNN in both R and Python
+
+--------
+The Contents of this Repository are as follows:
+
+1.KNN.R
+-- Made from scratch using R .. This code demonstrates the KNN Algorith using 
+the Iris dataset. Here i have selected 135 elements for training the algorithm
+and the remaining datasets for testing purpose:
+
+2.KNN.py
+-- This uses the standard machine learning libraries such as numpy,scikitlearn
+and mathplotlib to use the simplest machine learning algorithm KNN. We have 
+also plotted the graph for the better understanding of the dataset using the 
+mathplotlib function.
